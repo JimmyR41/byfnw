70908830883873194f85ce8d16382a19fa815641
Jimmy Riccelli <jrriccelli@gmail.com>
2017-01-09 00:48:33 -0500

    routed / to index.html && set static files

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var port = 8080;
 var router = require('./app/routes');
 app.use('/', router);
 
+//set static files
+app.use(express.static(__dirname + '/public'));
+
 //start server
 app.listen(port, function(req,res){
   console.log('Listening on 8080');
